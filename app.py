6c752acc06fafd51a2157edf25612a6a440323af
yasith <yasith.jayawardana@icloud.com>
2021-07-27 01:21:24 -0400

    implement robustify, refine UI

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import hashlib
 import io
+import json
 import os
 
+import requests
 from flask import Flask, render_template, request, redirect
 
 from extractor import Extractor
@@ -41,7 +43,21 @@ def upload_pdf():
 @app.route("/extract/<fp>", methods=['GET'])
 def extract_links(fp: str):
   urls = extractor.extract_urls(os.path.join(app.config['UPLOAD_FOLDER'], fp))
-  return render_template("urls.html", urls=urls)
+  return render_template("urls.html", filename=fp, urls=urls)
+
+
+@app.route("/robustify", methods=['POST'])
+def robustify():
+  urls = request.get_json()
+
+  def generate():
+    for url in urls:
+      yield f"Started: {url}\n"
+      res = requests.get(f"http://robustlinks.mementoweb.org/api/", {"url": url.strip()})
+      yield f"Processed: {url}\n"
+      yield f"{json.dumps(res.json())}\n"
+
+  return app.response_class(generate(), content_type='application/octet-stream')
 
 
 @app.route("/")
