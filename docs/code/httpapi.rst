622a9157ab04f7fe542ddcda10c01d52cdb80396
yasith <yasith.jayawardana@icloud.com>
2021-08-26 19:56:54 -0400

    use sphinxcontrib-httpdomain to generate API docs for http api

--- docs/code/httpapi.rst
+++ docs/code/httpapi.rst
@@ -3,5 +3,6 @@ HTTP API
 
 This module contains the implementation of the HTTP API which serves the Web Interface and its functionality.
 
-.. automodule:: pdflinks.api
-   :members:
+.. autoflask:: pdflinks.api:app
+   :endpoints:
+   :order: path
--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'sphinx.ext.autodoc'
+  'sphinx.ext.autodoc',
+  'sphinxcontrib.autohttp.flask'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
